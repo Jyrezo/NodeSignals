d2c2915a1a0034da8c06b8933d3ccfbe294bcbd6
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2023-01-10 08:46:10 -0500

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'NodeSignals',
   tagline: 'Dinosaurs are cool',
   url: 'https://codedjimmy.github.io',
-  baseUrl: '/nodesignals',
+  baseUrl: '/NodeSignals',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
